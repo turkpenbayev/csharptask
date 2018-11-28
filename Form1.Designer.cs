fda9d98d266705f05f0c106b6df1bd0df4544ce1
turkpenbayev <mr.turkpenbaev1997@gmail.com>
2018-11-28 14:56:07 +0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataSet1 = new task.DataSet1();
+            this.eMPBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.eMPTableAdapter = new task.DataSet1TableAdapters.EMPTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_download = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button_update = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eMPBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(660, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "клиенты";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // eMPBindingSource
+            // 
+            this.eMPBindingSource.DataMember = "EMP";
+            this.eMPBindingSource.DataSource = this.dataSet1;
+            // 
+            // eMPTableAdapter
+            // 
+            this.eMPTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(581, 376);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button_download
+            // 
+            this.button_download.Location = new System.Drawing.Point(660, 312);
+            this.button_download.Name = "button_download";
+            this.button_download.Size = new System.Drawing.Size(132, 27);
+            this.button_download.TabIndex = 3;
+            this.button_download.Text = "download";
+            this.button_download.UseVisualStyleBackColor = true;
+            this.button_download.Click += new System.EventHandler(this.button_download_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(663, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 26);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "credit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(722, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 28);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "info";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button_update
+            // 
+            this.button_update.Location = new System.Drawing.Point(663, 258);
+            this.button_update.Name = "button_update";
+            this.button_update.Size = new System.Drawing.Size(128, 33);
+            this.button_update.TabIndex = 6;
+            this.button_update.Text = "update data";
+            this.button_update.UseVisualStyleBackColor = true;
+            this.button_update.Click += new System.EventHandler(this.button_update_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(667, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(45, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 471);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_update);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_download);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eMPBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource eMPBindingSource;
+        private DataSet1TableAdapters.EMPTableAdapter eMPTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_download;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button_update;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.IO;
+
+namespace task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        OracleConnection con = new OracleConnection("User id=newuser;Password=password;Data Source=XE;Pooling=false;");
+        OracleCommandBuilder cmdbl;
+        OracleDataAdapter oda;
+        DataTable dt;
+        private void ToCsV(DataGridView dGV, string filename)
+        {
+            string stOutput = "";
+            // Export titles:
+            string sHeaders = "";
+
+            for (int j = 0; j < dGV.Columns.Count; j++)
+                sHeaders = sHeaders.ToString() + Convert.ToString(dGV.Columns[j].HeaderText) + "\t";
+            stOutput += sHeaders + "\r\n";
+            // Export data.
+            for (int i = 0; i < dGV.RowCount - 1; i++)
+            {
+                string stLine = "";
+                for (int j = 0; j < dGV.Rows[i].Cells.Count; j++)
+                    stLine = stLine.ToString() + Convert.ToString(dGV.Rows[i].Cells[j].Value) + "\t";
+                stOutput += stLine + "\r\n";
+            }
+            Encoding utf16 = Encoding.GetEncoding(1254);
+            byte[] output = utf16.GetBytes(stOutput);
+            FileStream fs = new FileStream(filename, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(output, 0, output.Length); //write the encoded file
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            con.Open();
+            oda = new OracleDataAdapter("select * from client order by id", con);
+            dt = new DataTable();
+            oda.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+
+        private void button_download_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Documents (*.xls)|*.xls";
+            sfd.FileName = "export.xls";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                //ToCsV(dataGridView1, @"c:\export.xls");
+                ToCsV(dataGridView1, sfd.FileName); // Here dataGridview1 is your grid view name
+                MessageBox.Show("Downloaded", "Succesfully!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            oda = new OracleDataAdapter("select * from credit order by id", con);
+            dt = new DataTable();
+            oda.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string client_id = textBox1.Text;
+            string query = "select client.name as client_name, client.salary as salary, credit.id as credit_id, credit_no,credit.amount from credit, client where credit.clinet_id="+client_id+ " and client.id=" + client_id + "order by client.id";
+            oda = new OracleDataAdapter(query, con);
+            dt = new DataTable();
+            oda.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+
+        private void button_update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmdbl = new OracleCommandBuilder(oda);
+                oda.Update(dt);
+                MessageBox.Show("updated", "Succesfullu saved!",MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error\n", ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
